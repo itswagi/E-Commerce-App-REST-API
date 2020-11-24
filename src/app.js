b6831f01c66277ff35e1f59dac2faf72a979c5c8
itswagi <43064115+itswagi@users.noreply.github.com>
2020-11-24 16:47:26 +0500

    Added babel

---
+++ src/app.js
@@ -0,0 +1,13 @@
+require('dotenv').config({path: '../.env'})
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+})
